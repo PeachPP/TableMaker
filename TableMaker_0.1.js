151b95a1d8a658f1e8d6a0c8ec97d6148b459f97
PeachPP <48002619+PeachPP@users.noreply.github.com>
2019-03-21 17:28:12 +0900

    add colum width attribute, fixed bug

--- TableMaker_0.1.js
+++ TableMaker_0.1.js
@@ -2,6 +2,7 @@
 TableMaker.js v0.1 | https://github.com/PeachPP/TableMaker | MIT license
 */
 
+
 function TableMaker(TableData) {
 	TableMaker.prototype.headerDataArray = new Array();
 	TableMaker.prototype.bodyDataArray;
@@ -21,14 +22,17 @@ function TableMaker(TableData) {
 
 		if(! arr[i].length) {
 			this.headerDataArray[depth] = this.headerDataArray[depth] ? this.headerDataArray[depth] : "";
+			
+			let childColCount = fn_getChildColsCount(arr[i]);
 
 			let rowspan		= arr[i].rowspan	?	" rowspan='"	+ arr[i].rowspan	+ "'" : "";
-			let colspan		= arr[i].childCols	?	" colspan='"	+ arr[i].childCols.length	+ "'" : "";
+			let colspan		= arr[i].childCols	?	" colspan='"	+ childColCount		+ "'" : "";
 			let styleClass	= arr[i].styleClass	?	" class='"		+ arr[i].styleClass	+ "'" : "";
 			let name		= arr[i].name		?	" name='"		+ arr[i].name		+ "'" : "";
 			let columnID	= arr[i].columnID	?	" id='"			+ arr[i].columnID	+ "'" : "";
+			let columnWidth	= arr[i].width		?	" width='"		+ arr[i].width		+ "'" : "";
 			let columnName	= arr[i].columnName;
-			let htmlTxt		= "<th" + columnID + name + styleClass + rowspan + colspan + ">" + columnName + "</th>";
+			let htmlTxt		= "<th" + columnID + name + styleClass + rowspan + colspan + columnWidth + ">" + columnName + "</th>";
 			this.headerDataArray[depth] += htmlTxt;
 
 			if(! arr[i].childCols) {
@@ -57,6 +61,22 @@ function TableMaker(TableData) {
 		return text;
 	};
 
+	const fn_getChildColsCount = (obj) => {
+		let count = 0;
+		
+		let children = obj.childCols || "";
+		if(! children)
+			return 1;
+		else {
+			count += children.length;
+			for(let i = 0; i < children.length; i++) {
+				if(children[i].childCols)
+					count += fn_getChildColsCount(children[i].childCols);
+			}
+		}
+		return count;
+	};
+	
 	const fn_makeRowData = (bodyData) => {
 		this.bodyDataArray = new Array();
 		let text = "";
@@ -102,8 +122,8 @@ function TableMaker(TableData) {
 		let thead = this.getHeaderText();
 		let tbody = this.getBodyText();
 
-		let text	 = "<table id='" + this.TableID + "' class='" + this.TableData.tableClass + "'>";
-			text	+= "<thead>";
+		let text	 = "<table id='" + this.TableID + "' class='" + (this.TableData.tableClass || "") + "'>";
+			text	+= "<thead class='" + (this.TableData.theadClass || "") + "'>";
 			text	+= thead;
 			text	+= "</thead>";
 			text	+= "<tbody>";
